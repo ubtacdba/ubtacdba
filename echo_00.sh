94d1bea9f8ef740f4dc137a3c8475b783502eef5
ubtacdba <92962886+ubtacdba@users.noreply.github.com>
2021-10-21 22:14:06 -0400

    Create echo_00.sh
    1st script ever to check into git

---
+++ echo_00.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/ksh
+
+echo "Hi, I’m @ubtacdba"
+
+echo "eyes I’m interested in ...learning to usr git"
+echo "seedling I’m currently learning among other things linux"
+echo "revolving_hearts I’m looking to collaborate on ..."
+echo "mailbox How to reach me ..."
